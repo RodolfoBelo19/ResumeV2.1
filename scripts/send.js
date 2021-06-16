9c48a24d3a8bb65354a94b4e5cd372b19c015b9d
RD Hunter <rodolfo belo>
2021-06-16 18:28:38 -0300

    Resume v2.1

---
+++ scripts/send.js
@@ -0,0 +1,23 @@
+/*const nodemailer = require('nodemailer');
+
+let remetente = nodemailer.createTransport({
+
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false,
+    auth:{
+        user: "rodolfo.silva.belo@gmail.com",
+        pass: "Rdtec1219*"}        
+});
+ 
+ 
+remetente.sendMail({
+    from: "rodolfo.silva.belo@gmail.com",
+    to: "rodolfo.silva.belo@gmail.com",
+    subject: "Backend",
+    text: "Serviço de envio de email está pronto!!! Mensagem enviada pelo node.js"
+}).then(message => {
+    console.log(message)
+}).catch(err => {
+    console.log(err)
+}) */
